5e89d11fb5f45749fb3659382aa2d62b52d10b71
kritika <kritikasri27@gmail.com>
2021-04-05 23:44:34 +0530

    Added Home component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -8,7 +9,10 @@ export default function Home() {
         <div class="head">
           <h1>Ready for new stuff</h1>
           <p>Buy new stock at reasonable cost</p>
-          <button>Get Started</button>
+
+          <Link to="/products">
+            <button>Get Started</button>
+          </Link>
         </div>
       </div>
       <div class="pic"></div>
