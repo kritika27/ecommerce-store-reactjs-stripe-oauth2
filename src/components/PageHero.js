6c6bec2a2baa0d2668929ed51e10e6427a071c4d
kritika <kritikasri27@gmail.com>
2021-04-25 21:51:23 +0530

    Added header component

---
+++ src/components/PageHero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./PageHero.css";
+
+const PageHero = ({ item, name }) => {
+  return (
+    <div className="heading-center">
+      <h3>
+        {name} / {item} Items
+      </h3>
+    </div>
+  );
+};
+
+export default PageHero;
