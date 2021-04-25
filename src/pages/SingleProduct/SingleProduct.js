13389395b925f81412e3487e4a357c77fe3ee459
kritika <kritikasri27@gmail.com>
2021-04-25 21:54:59 +0530

    Added Single Product detail component

---
+++ src/pages/SingleProduct/SingleProduct.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useProductsContext } from "../../context/products_context";
+import "./SingleProduct.css";
+import AddToCart from "../../components/cart/AddToCart";
+
+const SingleProduct = () => {
+  const { id } = useParams();
+  const { products } = useProductsContext();
+
+  const temp = products.filter((product) => product.id === id);
+
+  return (
+    <div>
+      {temp.map((products) => {
+        const { name, price, description, stock, image } = products;
+        return (
+          <div className="product-center" key={products.id}>
+            <img className="prod-photo" src={image} alt={name} />
+            <section className="content-prod">
+              <h2>{name}</h2>
+              <span class="fa fa-star review"></span>
+              <span class="fa fa-star review"></span>
+              <span class="fa fa-star review"></span>
+              <span class="fa fa-star review"></span>
+              <span class="fa fa-star-half-o review"></span>
+              <p>70 Reviews</p>
+              <h5 className="price-prod">${price}</h5>
+              <p className="description"> {description}</p>
+              <p className="info-prod">
+                <span>Available : </span>
+                In stock
+              </p>
+
+              <hr />
+              {{ stock } && <AddToCart product={products} />}
+            </section>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SingleProduct;
