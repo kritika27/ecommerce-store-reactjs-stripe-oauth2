568b2454025f64feb745ddc1b7528421b8143bff
kritika <kritikasri27@gmail.com>
2021-04-28 22:57:14 +0530

    Added Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-//import { FaBars } from 'react-icons/fa'
 import { useAuth0 } from "@auth0/auth0-react";
+import { useGlobalContext } from "../../context/cart_context";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const {
-    isLoading,
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
+  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
   const isUser = isAuthenticated && user;
+  const { amount } = useGlobalContext();
   return (
     <div className="nav">
-      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
-      {isUser && user.name && <h4>welcome</h4>}
       <div className="nav-container">
         <h1>Trends</h1>
 
@@ -28,10 +20,9 @@ export default function Navbar() {
           <Link to="/products">
             <li>Products</li>
           </Link>
+        </ul>
 
-          <Link to="/checkout">
-            <li>Checkout</li>
-          </Link>
+        <div className="user">
           {isUser ? (
             <button
               className="login"
@@ -44,13 +35,18 @@ export default function Navbar() {
               Log in
             </button>
           )}
-        </ul>
-
-        <i className="fa fa-bars bar-toggle"></i>
-
-        <Link to="/cart">
-          <i className="fa fa-shopping-cart fa-2x cart-icon"></i>
-        </Link>
+          <Link to="/cart">
+            <i className="fa fa-shopping-cart fa-2x cart-icon"></i>{" "}
+          </Link>
+          <div className="nav-item">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z" />
+            </svg>
+            <div className="amount-container">
+              <p className="total-amount">{amount}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
