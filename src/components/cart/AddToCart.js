254fb4402503f436e59ede68b61d89c6d6250d9a
kritika <kritikasri27@gmail.com>
2021-04-28 22:39:17 +0530

    Added add to cart component

---
+++ src/components/cart/AddToCart.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "../../context/cart_context";
+import AmountButtons from "../AmountButtons/AmountButtons";
+import "./Cart.css";
+
+const AddToCart = ({ product }) => {
+  const { addToCart } = useGlobalContext();
+  const { id, stock } = product;
+  console.log(product);
+
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
+
+  return (
+    <div className="btn-container">
+      <AmountButtons amount={amount} inc={increase} dec={decrease} />
+      <Link
+        to="/cart"
+        className="add-cart"
+        onClick={() => addToCart(id, amount, product)}
+      >
+        Add to cart
+      </Link>
+    </div>
+  );
+};
+
+export default AddToCart;
