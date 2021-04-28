08368fe4b648f48bd4a7f1b697d48f6982195909
kritika <kritikasri27@gmail.com>
2021-04-28 22:51:14 +0530

    Added main index file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-//import App from "./App";
-//import { ProductsProvider } from "./context/products_context";
-import Checkout from "./pages/Checkout";
-
 import { Auth0Provider } from "@auth0/auth0-react";
-//import Navbar from "./components/Navbar/Navbar";
 import App from "./App";
-import { CartProvider } from "./components/cart/cart_context";
-import { ProductsProvider } from "./pages/products_context";
-//dev-wt6m55wd.us.auth0.com
-//QynzIDzE9EBhVPMhOiWj8yNdI5jiZUhd
+import { CartProvider } from "./context/cart_context";
+import { ProductsProvider } from "./context/products_context";
+
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-wt6m55wd.us.auth0.com"
-    clientId="QynzIDzE9EBhVPMhOiWj8yNdI5jiZUhd"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
+    cacheLocation="localstorage"
   >
     <CartProvider>
       <ProductsProvider>
