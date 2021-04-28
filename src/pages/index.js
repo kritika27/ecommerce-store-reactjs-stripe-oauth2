5c1fbd804d3b74906d6c552d6fc94b2480869dfe
kritika <kritikasri27@gmail.com>
2021-04-29 00:44:20 +0530

    Fixed error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import CartItems from "./Cart/CartItems";
+import CartItems from "./CartItems/CartItems";
 import Error from "./Error/Error";
 import Home from "./Home/Home";
 import ProductList from "./Products/ProductList";
