97a5555679e2606f6136e0ccf8ce68459bc63ada
kritika <kritikasri27@gmail.com>
2021-05-05 08:10:30 +0530

    Added spinner and lazy loading logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {
-  CartItems,
-  Error,
-  Home,
-  ProductList,
-  SingleProduct,
-} from "./pages/index.js";
 import Navbar from "./components/Navbar/Navbar";
+import Spinner from "./components/Spinner/Spinner";
+
+const Cart = lazy(() => import("./pages/CartContent/CartItems"));
+const Homepage = lazy(() => import("./pages/Home/Home"));
+const Errorpage = lazy(() => import("./pages/Error/Error"));
+const Productlist = lazy(() => import("./pages/Products/ProductList"));
+const ProductDetails = lazy(() =>
+  import("./pages/SingleProduct/SingleProduct")
+);
 
 export default function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/cart" component={CartItems}></Route>
-        <Route exact path="/products" component={ProductList}></Route>
-        <Route exact path="/products/:id" children={<SingleProduct />}></Route>
-        <Route exact path="*" component={Error}></Route>
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+          <Route exact path="/cart" component={Cart}></Route>
+          <Route exact path="/products" component={Productlist}></Route>
+          <Route
+            exact
+            path="/products/:id"
+            children={<ProductDetails />}
+          ></Route>
+          <Route exact path="*" component={Errorpage}></Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
