76907b0e448aeeb6aaf75fe1de5b4d00d34e6588
kritika <kritikasri27@gmail.com>
2021-05-05 08:11:59 +0530

    Made cart layout responsive and display empty cart on having zero items in cart

--- src/pages/CartContent/CartItems.js
+++ src/pages/CartContent/CartItems.js
@@ -2,16 +2,38 @@ import React from "react";
 import { useGlobalContext } from "../../context/cart_context";
 import { Link } from "react-router-dom";
 import CartItem from "../../components/Cart/CartItem";
-import PageHero from "../../components/PageHero/PageHero";
 import StripePay from "../../components/Checkout/StripeCheckout";
 import "../../App.css";
 
 const CartItems = () => {
   const { cart, clear, total } = useGlobalContext();
-
+  if (cart.length < 1) {
+    return (
+      <div className="page-100">
+        <div className="empty" style={{ textAlign: "center" }}>
+          <h2
+            style={{
+              marginBottom: "1rem",
+              marginTop: "1.5rem",
+            }}
+          >
+            Your cart is empty.
+          </h2>
+          <Link
+            to="/products"
+            className="link-btn"
+            
+          >
+            Shop Now
+          </Link>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
-      <PageHero name="CART" item={cart.length} />
+      <h1 style={{ textAlign: "center", paddingTop: "0.5rem" }}>Cart</h1>
+
       <div className="section section-center">
         {cart.map((item) => {
           return <CartItem key={item.id} {...item} />;
@@ -31,7 +53,7 @@ const CartItems = () => {
               Order Total : <span>${total}</span>
             </h3>
           </article>
-          <StripePay price={total} />*
+          <StripePay price={total} />
         </div>
       </div>
     </>
