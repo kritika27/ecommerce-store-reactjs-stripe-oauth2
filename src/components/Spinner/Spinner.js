afd42a0762ea36975e60b1085d3e431f64962b39
kritika <kritikasri27@gmail.com>
2021-05-05 08:07:52 +0530

    Added spinner for loading data

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => (
+  <div className="spinner-overlay">
+    <div className="spinner-container"></div>
+  </div>
+);
+
+export default Spinner;
